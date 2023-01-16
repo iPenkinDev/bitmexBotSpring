11153ff13efe9ad763a756664fea55a64f81af2a
Igor Penkin <ipenkin86@gmail.com>
2023-01-16 18:54:00 +0300

    add Bot model

---
+++ src/main/java/com/bitmexBotSpring/model/Bot.java
@@ -0,0 +1,13 @@
+package com.bitmexBotSpring.model;
+
+import lombok.Data;
+
+@Data
+public class Bot {
+    private String apiKey;
+    private String apiSecret;
+    private double step;
+    private int level;
+    private double coef;
+
+}
