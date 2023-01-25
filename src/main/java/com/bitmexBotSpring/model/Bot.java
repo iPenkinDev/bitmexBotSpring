f26acb69efcf7ddda59c5dedd50a9d8e3a6e010f
Igor Penkin <igor.penkin.dev@gmail.com>
2023-01-25 14:58:59 +0300

    add Bot class

--- src/main/java/com/bitmexBotSpring/model/Bot.java
+++ src/main/java/com/bitmexBotSpring/model/Bot.java
@@ -1,8 +1,13 @@
 package com.bitmexBotSpring.model;
 
 import lombok.Data;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.stereotype.Component;
 
 @Data
+@Component
 public class Bot {
     private String apiKey;
     private String apiSecret;
